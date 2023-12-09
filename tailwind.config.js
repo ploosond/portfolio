aed24066e74e555b7d33e2f7f4a89ecc3437be74
Prajwol Devkota <prajwol.devkota016@gmail.com>
2023-12-09 07:45:03 +0100

    removed ghpage deployment config as of bad result

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,6 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      // Set your custom default color here
-      default: '#4ade80', // Replace with your desired color value
-      // ... (other color definitions)
-    },
     extend: {
       fontFamily: {
         sans: ['Poppins', 'sans-serif'],
